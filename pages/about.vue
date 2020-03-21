23da1a68f9b3e2486d453160b2d200158c116b65
Lennard Bredenkamp <lennard.bredenkamp@gmail.com>
2020-03-21 19:58:11 +0100

    Added pages, added basic credits (illustration).

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fluid>
+
+    <h1>About</h1>
+    <h2>Credits</h2>
+    <p>
+      Illustration: <a href="https://www.noramarleen.de/">Nora Marleen</a>
+    </p>
+    <h2>Tech</h2>
+   </v-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
---
+++ pages/privacy.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid>
+
+  <h1>Privacy</h1>
+   </v-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
