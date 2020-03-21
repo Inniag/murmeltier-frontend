b86f8ff3e96fc837e39451f53e16249c2ae33e3c
Lennard Bredenkamp <lennard.bredenkamp@gmail.com>
2020-03-21 22:10:26 +0100

    Added hashtag field, starting mood in the middle now.

--- pages/mood.vue
+++ pages/mood.vue
@@ -8,27 +8,35 @@
         height="400"
         hide-delimiter-background
         show-arrows-on-hover
+        v-model="model"
     >
         <v-carousel-item
         v-for="(slide, i) in slides"
         :key="i"
         >
         <v-sheet
-            :color="colors[i]"
+            light
             height="100%"
         >
             <v-row
             class="fill-height"
-            align="center"
             justify="center"
             >
-            <!-- <div class="display-3">{{ slide }} Slide</div> -->
-            <img width="200px" height="auto" :src="require('../assets/mood-'+(i+1)+'.svg')" />
-            <!-- <v-img :src="require('../assets/sample_image.jpg')"/> -->
+                <img width="200px" height="auto" :src="require('../assets/mood-'+(i+1)+'.svg')" />
             </v-row>
         </v-sheet>
         </v-carousel-item>
     </v-carousel>
+
+    <v-text-field :rules="rules"></v-text-field>
+
+    <div class="text-center">
+        <v-btn class="ma-2" outlined color="indigo" dark>Abbrechen</v-btn>
+
+        <v-btn class="ma-2" color="success">
+            OK
+        </v-btn>
+    </div>
    </v-container>
 </template>
 
@@ -39,19 +47,21 @@ export default {
     },  
     data () {
         return {
-            colors: [
-                'indigo',
-                'warning',
-                'pink darken-2',
-                'red lighten-1',
-                'deep-purple accent-4',
-            ],
             slides: [
                 'First',
                 'Second',
                 'Third',
                 'Fourth',
                 'Fifth',
+            ],
+            model: 2,
+            rules: [
+                value => !!value || 'Required.',
+                value => (value || '').length <= 60 || 'Maximal 60 Zeichen',
+                // value => {
+                // // const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                // // return pattern.test(value) || 'Invalid e-mail.'
+                // },
             ],
         }
   }
