e0091925a990f0ecc7292d05d14091fd240f96c2
Lennard Bredenkamp <lennard.bredenkamp@gmail.com>
2020-03-22 12:18:40 +0100

    Added dummy page for chat.

---
+++ pages/chat.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container fluid>
+        <h1>Chat!</h1>
+   </v-container>
+</template>
+
+<script>
+
+export default {
+
+    components: {
+    },  
+    data () {
+        return {
+            
+        }
+  },
+
+
+  methods: {
+    submitMurmel() {
+        alert("Submitted!")
+    }
+  }
+}
+</script>
+
